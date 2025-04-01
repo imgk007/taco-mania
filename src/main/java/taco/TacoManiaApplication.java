011b6704308df6ed00d6ab7903ccbf3f71cdf56d
imgk007 <gnanavelkumarang7@gmail.com>
2025-04-01 23:34:33 +0530

    first commit

---
+++ src/main/java/taco/TacoManiaApplication.java
@@ -0,0 +1,13 @@
+package taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoManiaApplication.class, args);
+	}
+
+}
