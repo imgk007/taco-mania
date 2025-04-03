d9036a9c2411e53681548c7bdb47c2c2077d24c9
imgk007 <gnanavelkumarang7@gmail.com>
2025-04-03 23:48:51 +0530

    Added test class for controller

--- src/main/java/taco/HomeController.java
+++ src/main/java/taco/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 @GetMapping("/")
     public String home() {
+    // the return value, 'home' is interpreted as logical name of a view,
+    // i.e., it represents the name of HTML file which we are going to implement
     return "home";
-}
+    }
 }
